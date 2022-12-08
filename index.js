38ab81d9f1ef0d094f65d6632ec16162e28f0030
Nikhilesh Katakam <nikhilesh.k@gmail.com>
2022-12-07 16:37:48 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/i18n/api/v2/combination/translationsAndPattern', function requestHandler(req, res) {
+    res.json({message: "Its Working!!!"})
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
